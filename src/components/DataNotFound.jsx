6a1f83d7a728c9f347944c82fd2f15c831ba07fe
mustafa7pato <m.pato891@gmail.com>
2023-04-07 19:47:20 +0200

    done

--- src/components/DataNotFound.jsx
+++ src/components/DataNotFound.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const DataNotFound = () => {
   return (
-    <div className="flex error w-full justify-center align-middle">
+    <div className="flex error sm:w-6/12 md:w-full justify-center align-middle">
       <h1 className="font-extrabold text-slate-900 text-4xl">
         لا يوجد اصناف حاليا
       </h1>
