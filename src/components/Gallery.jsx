3889176ba5aa2a3f875c1707c8c3177e3964b317
mustafa7pato <m.pato891@gmail.com>
2023-04-09 03:55:32 +0200

    do

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -45,12 +45,11 @@ const Gallery = () => {
               return (
                 <div key={index}>
                   {" "}
-                  {/* Key added to parent div */}
-                  <div className="h-80 w-96 sm:p-3 my-4">
+                  <div className="h-80 img-gallery w-96 sm:p-3 my-4">
                     <img
                       src={img.image}
                       alt=""
-                      className="w-full img-gallery  h-full shadow-xl rounded-2xl transition-all duration-300 transform-gpu hover:scale-105 shadow-md hover:shadow-lg cursor-pointer"
+                      className="w-full   h-full shadow-xl rounded-2xl transition-all duration-300 transform-gpu hover:scale-105 shadow-md hover:shadow-lg cursor-pointer"
                     />
                   </div>
                 </div>
